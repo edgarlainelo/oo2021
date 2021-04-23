e3d613b0241e84284db7b1d18521b6f52dc395f2
raimop <ray@tlu.ee>
2021-04-23 13:40:50 +0300

    Can fetch all users and add new user

--- w10_tunnirakenduse_lahendus/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ w10_tunnirakenduse_lahendus/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controllers;
 
+import javax.validation.Valid;
+
+import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -21,5 +26,20 @@ public class UserController {
   public String showUserList(Model model){
     model.addAttribute("users", userRepository.findAll());
     return "index";
+  }
+
+  @GetMapping("/signup")
+  public String showSignUp(User user){
+    return "signup";
+  }
+
+  @PostMapping("/addUser")
+  public String addUser(@Valid User user, BindingResult result, Model model){
+    if (result.hasErrors()){
+      return "signup";
+    }
+
+    userRepository.save(user);
+    return "redirect:/index";
   }
 }
