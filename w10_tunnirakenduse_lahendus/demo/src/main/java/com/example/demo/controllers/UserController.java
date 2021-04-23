9c93594a339a253016ac9ca7137dbf2f89d2fa3a
raimop <ray@tlu.ee>
2021-04-23 14:44:29 +0300

    Added update endpoint and view

--- w10_tunnirakenduse_lahendus/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ w10_tunnirakenduse_lahendus/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -40,6 +41,36 @@ public class UserController {
     }
 
     userRepository.save(user);
+    return "redirect:/index";
+  }
+
+  @GetMapping("/edit/{id}")
+  public String showUpdate(@PathVariable("id") long id, Model model){
+    User user = userRepository.findById(id)
+      .orElseThrow(() -> new IllegalArgumentException("No such user with id " + id));
+    model.addAttribute("user", user);
+
+    return "update";
+  }
+
+  @PostMapping("/update/{id}")
+  public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model){
+    if (result.hasErrors()){
+      return "update";
+    }
+
+    userRepository.save(user);
+
+    return "redirect:/index";
+  }
+
+  @GetMapping("/delete/{id}")
+  public String deleteUser(@PathVariable("id") long id, Model model){
+    User user = userRepository.findById(id)
+      .orElseThrow(() -> new IllegalArgumentException("No such user with id " + id));
+
+    userRepository.delete(user);
+
     return "redirect:/index";
   }
 }
